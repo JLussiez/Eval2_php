f67b511e39074bebe53d6a9e5b12684f6036488a
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2023-04-05 15:00:58 +0200

    Delete eval_02_php_les_basiques_etudiant directory

--- eval_02_php_les_basiques_etudiant/eval-02/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function(){
-    let errormsg = document.getElementById('errormsg');
-    if(errormsg){
-        let errormsgModal = new bootstrap.Modal(errormsg, {
-            keyboard: false
-        });    
-        errormsgModal.show();
-    }
-});
